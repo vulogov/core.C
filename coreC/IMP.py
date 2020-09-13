2b405f133028fabf5569d781017321e947aa3489
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-09-13 01:27:14 -0700

    Dev #3

--- coreC/IMP.py
+++ coreC/IMP.py
@@ -7,14 +7,14 @@ class IMP(object):
         pass
     def Reload(self):
         pass
-    def __ImportModule(ns, _module):
+    def __ImportModule(self, _module):
         _attrs = ['_lib', '_mkdir', '_set', '_tpl']
-        _mod = Dict()
+        _mod = {}
         for a in _attrs:
             if hasattr(_module, a) is True:
                 _mod[a] = getattr(_module, a)
         return _mod
-    def Import(self, name):
+    def Import(self, module):
         for loader, module_name, is_pkg in pkgutil.walk_packages(importlib.import_module(module).__path__):
             _module = loader.find_module(module_name).load_module(module_name)
-            _mod = __ImportModule(ns, _module)
+            _mod = self.__ImportModule(_module)
